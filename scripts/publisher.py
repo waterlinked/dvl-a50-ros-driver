1ee418cd9bf403d75c442056d9ee15209e0c9721
Onur Bagoren <onurbagoren3@gmail.com>
2022-06-07 17:56:33 -0400

    local position included

--- scripts/publisher.py
+++ scripts/publisher.py
@@ -4,20 +4,24 @@ import json
 import rospy
 from time import sleep
 from std_msgs.msg import String
+from geometry_msgs.msg import QuaternionStamped
 from waterlinked_a50_ros_driver.msg import DVL
 from waterlinked_a50_ros_driver.msg import DVLBeam
 import select
+from scipy.spatial.transform import Rotation as R
+
 
 def connect():
-	global s, TCP_IP, TCP_PORT
-	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect((TCP_IP, TCP_PORT))
-		s.settimeout(1)
-	except socket.error as err:
-		rospy.logerr("No route to host, DVL might be booting? {}".format(err))
-		sleep(1)
-		connect()
+    global s, TCP_IP, TCP_PORT
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((TCP_IP, TCP_PORT))
+        s.settimeout(1)
+    except socket.error as err:
+        rospy.logerr("No route to host, DVL might be booting? {}".format(err))
+        sleep(1)
+        connect()
+
 
 oldJson = ""
 
@@ -27,102 +31,127 @@ beam1 = DVLBeam()
 beam2 = DVLBeam()
 beam3 = DVLBeam()
 
-def getData():
-	global oldJson, s
-	raw_data = ""
 
-	while not '\n' in raw_data:
-		try:
-			rec = s.recv(1) # Add timeout for that
-			if len(rec) == 0:
-				rospy.logerr("Socket closed by the DVL, reopening")
-				connect()
-				continue
-		except socket.timeout as err:
-			rospy.logerr("Lost connection with the DVL, reinitiating the connection: {}".format(err))
-			connect()
-			continue
-		raw_data = raw_data + rec
-	raw_data = oldJson + raw_data
-	oldJson = ""
-	raw_data = raw_data.split('\n')
-	oldJson = raw_data[1]
-	raw_data = raw_data[0]
-	return raw_data
+def getData():
+    global oldJson, s
+    raw_data = ""
+
+    while not '\n' in raw_data:
+        try:
+            rec = s.recv(1)  # Add timeout for that
+            if len(rec) == 0:
+                rospy.logerr("Socket closed by the DVL, reopening")
+                connect()
+                continue
+        except socket.timeout as err:
+            rospy.logerr(
+                "Lost connection with the DVL, reinitiating the connection: {}".format(err))
+            connect()
+            continue
+        raw_data = raw_data + rec
+    raw_data = oldJson + raw_data
+    oldJson = ""
+    raw_data = raw_data.split('\n')
+    oldJson = raw_data[1]
+    raw_data = raw_data[0]
+    return raw_data
 
 
 def publisher():
-	pub_raw = rospy.Publisher('dvl/json_data', String, queue_size=10)
-	pub = rospy.Publisher('dvl/data', DVL, queue_size=10)
+    pub_raw = rospy.Publisher('dvl/json_data', String, queue_size=10)
+    pub = rospy.Publisher('dvl/data', DVL, queue_size=10)
 
-	rate = rospy.Rate(10) # 10hz
-	while not rospy.is_shutdown():
-		raw_data = getData()
-		if do_log_raw_data:
-			rospy.loginfo(raw_data)
-		data = json.loads(raw_data)
-		if data["type"] != "velocity":
-			continue
-		pub_raw.publish(raw_data)
+    rate = rospy.Rate(10)  # 10hz
+    while not rospy.is_shutdown():
+        raw_data = getData()
+        if do_log_raw_data:
+            rospy.loginfo(raw_data)
+        data = json.loads(raw_data)
+        pub_raw.publish(raw_data)
+        if data["type"] != "velocity":
+            theDVL.header.stamp = rospy.Time.now()
+            theDVL.header.frame_id = "dvl_link"
+            theDVL.time = data["time"]
+            theDVL.velocity.x = data["vx"]
+            theDVL.velocity.y = data["vy"]
+            theDVL.velocity.z = data["vz"]
+            cov = data["covariance"]
+            theDVL.velocity_covariance[0] = cov[0]
+            theDVL.velocity_covariance[1] = cov[1]
+            theDVL.velocity_covariance[2] = cov[2]
+            theDVL.fom = data["fom"]
+            theDVL.altitude = data["altitude"]
+            theDVL.velocity_valid = data["velocity_valid"]
+            theDVL.status = data["status"]
+            theDVL.form = data["format"]
 
-		theDVL.header.stamp = rospy.Time.now()
-		theDVL.header.frame_id = "dvl_link"
-		theDVL.time = data["time"]
-		theDVL.velocity.x = data["vx"]
-		theDVL.velocity.y = data["vy"]
-		theDVL.velocity.z = data["vz"]
-		cov = data["covariance"]
-		theDVL.velocity_covariance[0] = cov[0]
-		theDVL.velocity_covariance[1] = cov[1]
-		theDVL.velocity_covariance[2] = cov[2]
-		theDVL.fom = data["fom"]
-		theDVL.altitude = data["altitude"]
-		theDVL.velocity_valid = data["velocity_valid"]
-		theDVL.status = data["status"]
-		theDVL.form = data["format"]
+            beam0.id = data["transducers"][0]["id"]
+            beam0.velocity = data["transducers"][0]["velocity"]
+            beam0.distance = data["transducers"][0]["distance"]
+            beam0.rssi = data["transducers"][0]["rssi"]
+            beam0.nsd = data["transducers"][0]["nsd"]
+            beam0.valid = data["transducers"][0]["beam_valid"]
 
-		beam0.id = data["transducers"][0]["id"]
-		beam0.velocity = data["transducers"][0]["velocity"]
-		beam0.distance = data["transducers"][0]["distance"]
-		beam0.rssi = data["transducers"][0]["rssi"]
-		beam0.nsd = data["transducers"][0]["nsd"]
-		beam0.valid = data["transducers"][0]["beam_valid"]
+            beam1.id = data["transducers"][1]["id"]
+            beam1.velocity = data["transducers"][1]["velocity"]
+            beam1.distance = data["transducers"][1]["distance"]
+            beam1.rssi = data["transducers"][1]["rssi"]
+            beam1.nsd = data["transducers"][1]["nsd"]
+            beam1.valid = data["transducers"][1]["beam_valid"]
 
-		beam1.id = data["transducers"][1]["id"]
-		beam1.velocity = data["transducers"][1]["velocity"]
-		beam1.distance = data["transducers"][1]["distance"]
-		beam1.rssi = data["transducers"][1]["rssi"]
-		beam1.nsd = data["transducers"][1]["nsd"]
-		beam1.valid = data["transducers"][1]["beam_valid"]
+            beam2.id = data["transducers"][2]["id"]
+            beam2.velocity = data["transducers"][2]["velocity"]
+            beam2.distance = data["transducers"][2]["distance"]
+            beam2.rssi = data["transducers"][2]["rssi"]
+            beam2.nsd = data["transducers"][2]["nsd"]
+            beam2.valid = data["transducers"][2]["beam_valid"]
 
-		beam2.id = data["transducers"][2]["id"]
-		beam2.velocity = data["transducers"][2]["velocity"]
-		beam2.distance = data["transducers"][2]["distance"]
-		beam2.rssi = data["transducers"][2]["rssi"]
-		beam2.nsd = data["transducers"][2]["nsd"]
-		beam2.valid = data["transducers"][2]["beam_valid"]
+            beam3.id = data["transducers"][3]["id"]
+            beam3.velocity = data["transducers"][3]["velocity"]
+            beam3.distance = data["transducers"][3]["distance"]
+            beam3.rssi = data["transducers"][3]["rssi"]
+            beam3.nsd = data["transducers"][3]["nsd"]
+            beam3.valid = data["transducers"][3]["beam_valid"]
 
-		beam3.id = data["transducers"][3]["id"]
-		beam3.velocity = data["transducers"][3]["velocity"]
-		beam3.distance = data["transducers"][3]["distance"]
-		beam3.rssi = data["transducers"][3]["rssi"]
-		beam3.nsd = data["transducers"][3]["nsd"]
-		beam3.valid = data["transducers"][3]["beam_valid"]
+            theDVL.beams = [beam0, beam1, beam2, beam3]
 
-		theDVL.beams = [beam0, beam1, beam2, beam3]
+            pub.publish(theDVL)
 
-		pub.publish(theDVL)
+        elif data["type"] != "position_local":
+            roll = data["roll"]
+            pitch = data["pitch"]
+            yaw = data["yaw"]
+            std = data["std"]
 
-		rate.sleep()
+            quat = QuaternionStamped()
+            quat.header.stamp = rospy.Time.now()
+            quat.header.frame_id = "dvl_link"
+
+            rot = R.from_euler('xyz', [roll, pitch, yaw])
+            qx = rot.as_quat()[0]
+            qy = rot.as_quat()[1]
+            qz = rot.as_quat()[2]
+            qw = rot.as_quat()[3]
+            quat.quaternion.x = qx
+            quat.quaternion.y = qy
+            quat.quaternion.z = qz
+            quat.quaternion.w = qw
+
+            pub_quat = rospy.Publisher(
+                'dvl/quaternion', QuaternionStamped, queue_size=10)
+            pub_quat.publish(quat)
+
+        rate.sleep()
+
 
 if __name__ == '__main__':
-	global s, TCP_IP, TCP_PORT, do_log_raw_data
-	rospy.init_node('a50_pub', anonymous=False)
-	TCP_IP = rospy.get_param("~ip", "10.42.0.186")
-	TCP_PORT = rospy.get_param("~port", 16171)
-	do_log_raw_data = rospy.get_param("~do_log_raw_data", False)
-	connect()
-	try:
-		publisher()
-	except rospy.ROSInterruptException:
-		s.close()
+    global s, TCP_IP, TCP_PORT, do_log_raw_data
+    rospy.init_node('a50_pub', anonymous=False)
+    TCP_IP = rospy.get_param("~ip", "10.42.0.186")
+    TCP_PORT = rospy.get_param("~port", 16171)
+    do_log_raw_data = rospy.get_param("~do_log_raw_data", False)
+    connect()
+    try:
+        publisher()
+    except rospy.ROSInterruptException:
+        s.close()
